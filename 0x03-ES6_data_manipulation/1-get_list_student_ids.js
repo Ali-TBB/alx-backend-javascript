348b70727d6a8d36eac28920cb2a48841d5109d1
Ali-TBB <Ali-Tbb@git.com>
2024-07-30 21:25:57 +0100

    all done

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,7 @@
 export default function getListStudentIds(arrOfObjs) {
-    if (!(Array.isArray(arrOfObjs))) {
-      return [];
-    }
-  
-    return arrOfObjs.map((obj) => obj.id);
+  if (!(Array.isArray(arrOfObjs))) {
+    return [];
+  }
+
+  return arrOfObjs.map((obj) => obj.id);
 }
